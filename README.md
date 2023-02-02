815fc77a4c8290fb4d0eb565283db056577bad33
diversable <dan.mantei@outlook.com>
2023-02-01 16:13:28 -0800

    update prototype and README.md

--- README.md
+++ README.md
@@ -3,3 +3,5 @@ This software is a prototype.
 Currently, Julia language must already be installed and on the user's path for this software to function properly.
 
 Also, the user presently has to manually copy the "./src/julia/Gaston.jl" file to `$HOME/.julia/gaston/Gaston.jl` for the binary to function.
+
+Otherwise, the cli has some useful functionality already - feel free to give it a try and give feedback on the API!
--- src/main.rs
+++ src/main.rs
@@ -77,21 +77,19 @@ fn main() {
     let home_dir = home_dir().expect("Couldn't find the user's home directory");
     let julia_dir = PathBuf::from(".julia/gaston/Gaston.jl");
     let mut gaston_path = PathBuf::new();
-    // gaston_path.push(home_dir);
+    gaston_path.push(home_dir);
     gaston_path.push(julia_dir);
 
     // Include some custom code defined in <file>.
     // This is safe because the included code doesn't do any strange things.
     unsafe {
-        // let path = PathBuf::from("/home/danamantei/.julia/gaston/Gaston.jl");
-
         if gaston_path.exists() {
             println!("Gaston path exists @: {:?}", gaston_path);
             julia.include(gaston_path).expect("Could not include file");
         } else {
             julia
-                // .include("/home/danamantei/.julia/gaston/Gaston.jl")
-                .include("./src/julia/Gaston.jl")
+                // TODO! Fix this! access to local path is broken...
+                .include("julia/Gaston.jl")
                 .expect("Else path: Could not include file");
         }
     }
